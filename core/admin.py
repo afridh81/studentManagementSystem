af343729c961b8be0ac7a7ef0e84abd4ec6b131a
afridh81 <105249272+afridh81@users.noreply.github.com>
2022-08-09 13:10:56 +0530

    Add files via upload

---
+++ core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Subject_model)
+admin.site.register(Student)
+admin.site.register(Department)
+admin.site.register(Profile)
+
+# admin.site.register(Answer_details)
+# admin.site.register(Answer_key_details)
+# admin.site.register(Score_model)
